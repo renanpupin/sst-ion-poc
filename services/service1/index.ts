8830542e284ee18417dfc17e0296a30f511c9b66
Renan Pupin <renan.pupin@gmail.com>
2024-03-27 12:23:33 +0100

    add topic for testing

--- services/service1/index.ts
+++ services/service1/index.ts
@@ -1,10 +1,34 @@
-export const handler = async (event) => {
-    console.log(event);
+import {Resource} from 'sst'
+import {SNSClient, PublishCommand} from '@aws-sdk/client-sns'
+const client = new SNSClient()
+
+export const handler = async event => {
+    console.log(event)
 
     const message = `Hello, from service 1! (${process.env.SST_LIVE ? 'live' : 'not live'})`
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ route: event.routeKey, status: "ok", message: message }, null, 2),
-    };
-};
+        body: JSON.stringify({route: event.routeKey, status: 'ok', message: message}, null, 2)
+    }
+}
+
+export const publisher = async event => {
+    await client.send(
+        new PublishCommand({
+            TargetArn: Resource.MyTopic.arn,
+            Message: JSON.stringify({foo: 'bar'}),
+            MessageAttributes: {foo: {DataType: 'String', StringValue: 'bar'}}
+        })
+    )
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({status: 'sent'}, null, 2)
+    }
+}
+
+export const subscriber = async event => {
+    console.log('subscriber', event)
+    return 'ok'
+}
--- sst.config.ts
+++ sst.config.ts
@@ -1,16 +1,35 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "my-ts-app",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
-    };
-  },
-  async run() {
-    new sst.aws.ApiGatewayV2("MyApi")
-        .route("GET /", "services/service1/index.handler")
-        .route("GET /service2", "services/service2/index.handler")
-  },
-});
+    app(input) {
+        return {
+            name: 'my-ts-app',
+            removal: input?.stage === 'production' ? 'retain' : 'remove',
+            home: 'aws'
+        }
+    },
+    async run() {
+        const topic = new sst.aws.SnsTopic('MySnsTopic')
+        topic.subscribe('services/service1/index.subscriber')
+
+        // const publisherApp = new sst.aws.Function('Publisher', {
+        //     handler: 'services/service1/index.publisher',
+        //     link: [topic],
+        //     url: true
+        // })
+
+        const apiGatewayApp = new sst.aws.ApiGatewayV2('MyApi')
+            .route('GET /', 'services/service1/index.handler')
+            .route('GET /publisher', {
+                handler: 'services/service1/index.publisher',
+                link: [topic] //this is not working
+            })
+            .route('GET /service2', 'services/service2/index.handler')
+
+        return {
+            // app: publisherApp.url,
+            apiGatewayApp: apiGatewayApp.url,
+            topic: topic.name
+        }
+    }
+})
